4751c11536812116472f09b75ea6bb04d21843a2
dbassett7 <dbassett7@gatech.edu>
2020-08-12 17:33:45 -0400

    noodling around with parsing the PDF file

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ file = open('2020SV-00052-R00HB06004-SV.PDF', 'rb')
 fileReader = PyPDF2.PdfFileReader(file)
 
 page = fileReader.getPage(0)
-page_content = page.extractText()
-print(page_content.encode('utf-8'))
+page_content = page.extractText() # .encode('utf-8')
+page_content = page_content.replace('\n', '')
+# print(page_content)
+separator = 'The following is the roll call vote:'
+after_separator = page_content.split(separator, 1)[1]
+# print(after_separator)
+# post_split = after_separator.split('  ')
+# print(post_split)
+
