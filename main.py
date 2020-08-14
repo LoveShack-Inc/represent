86f28af4654c227ffb8e34e067cd23711771c28b
dbassett7 <dbassett7@gatech.edu>
2020-08-14 18:09:23 -0400

    Made a very dirty regex to scan the PDF

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import PyPDF2
+import re
 
 file = open('2020SV-00052-R00HB06004-SV.PDF', 'rb')
 
 fileReader = PyPDF2.PdfFileReader(file)
 
 page = fileReader.getPage(0)
-page_content = page.extractText() # .encode('utf-8')
+page_content = page.extractText()
 page_content = page_content.replace('\n', '')
-# print(page_content)
 separator = 'The following is the roll call vote:'
 after_separator = page_content.split(separator, 1)[1]
-# print(after_separator)
-# post_split = after_separator.split('  ')
-# print(post_split)
+after_separator += '\n'
 
+reg_1 = re.compile('( Y | N )(.*?)(?= Y | N | \n)')
+reg_result = re.findall(reg_1, after_separator)
+reg_2 = re.compile('[a-zA-Z ]')
+
+vote_list = []
+
+for i in range(len(reg_result)):
+    t1 = reg_result[i][0]
+    t2 = "".join(re.findall(reg_2, reg_result[i][1])).strip()
+    vote_list.append((t1, t2))
+
+hi = 0
