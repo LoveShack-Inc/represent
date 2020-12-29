6f76ea10657265706782fa61a013498e7af1c42b
dalton-b <daltonbassett@gmail.com>
2020-12-29 12:22:39 -0500

    Added BillInfoDao, BillInfo dataclass

--- rep/db/db_helper.py
+++ rep/db/db_helper.py
@@ -71,5 +71,51 @@ def representative_info():
     conn.close()
     logging.info("Representative Info table initialized")
 
+def bill_info():
+    kwargs = {}
+    conn = kwargs.get('database', sqlite_get_connection_helper())
+
+    c = conn.cursor()
+
+    logging.info("Initializg Bill Info table...")
+    c.executescript('''
+
+            CREATE TABLE IF NOT EXISTS bill_info
+                (
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    billNum VARCHAR NOT NULL,
+                    lcoNum INTEGER NOT NULL,
+                    sessYear INTEGER NOT NULL,
+                    sessNum INTEGER NOT NULL,
+                    typeCode VARCHAR NOT NULL,
+                    billTitle VARCHAR NOT NULL,
+                    stmtPurp VARCHAR NULL,
+                    emergencyCert BOOLEAN NOT NULL,
+                    raised BOOLEAN NOT NULL,
+                    proposed BOOLEAN NOT NULL,
+                    subBill BOOLEAN NOT NULL,
+                    nomination BOOLEAN NOT NULL,
+                    numPages INTEGER NOT NULL,
+                    houseCalNum VARCHAR NULL,
+                    senateCalNum VARCHAR NULL,
+                    filedDate VARCHAR NOT NULL,
+                    readIntoFloor VARCHAR NOT NULL,
+                    petitionNum VARCHAR NULL,
+                    pasaNum INTEGER NULL,
+                    pasaType VARCHAR NULL,
+                    senateAmd VARCHAR NULL,
+                    houseAmd VARCHAR NULL
+                );
+
+    ''')
+
+    # https://www.cga.ct.gov/ftp/pub/data/bill_info.csv
+    df = pd.read_csv('database/data/bill_info_2020.csv')
+    df.to_sql('bill_info', conn, if_exists='append', index=False)
+
+    conn.commit()
+    conn.close()
+    logging.info("Bill Info table initialized")
+
 representative_info()
 bill_info()
